70401afebad26cb041650bb0647de63eddc02abf
marcuscastelo <marcuscastelo@usp.br>
2022-04-05 10:16:09 -0300

    Translation + Rotation

--- with_gui.py
+++ with_gui.py
@@ -28,18 +28,51 @@ class MainWindow(gui.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.scale = 1.0
+        self.rotation_angle = 0.0
+        self.translation_x = 0.0
+        self.translation_y = 0.0
 
-    def _on_value_changed(self):
+        self.scale_mat = np.eye(4, dtype=np.float32)
+        self.rotation_mat = np.eye(4, dtype=np.float32)
+        self.translation_mat = np.eye(4, dtype=np.float32)
+
+    def _on_scale_changed(self):
         # Recalculate MVP
-        mvp = np.eye(4, dtype=np.float32)
-        mvp = np.dot(mvp, np.diag([self.scale, self.scale, 1, 1]))
-        STATE.mvp = mvp
+        self.scale_mat = np.diag([self.scale, self.scale, 1, 1.0])
+        self._update_mvp()
         pass
+
+    def _on_translation_changed(self):
+        # Recalculate MVP
+        self.translation_mat = np.diag([1, 1, 1, 1.0])
+        self.translation_mat[0, 3] = self.translation_x
+        self.translation_mat[1, 3] = self.translation_y
+        self._update_mvp()
+        pass
+
+    def _on_rotation_changed(self):
+        # Recalculate MVP
+        self.rotation_mat = np.eye(4, dtype=np.float32)
+        self.rotation_mat[0, 0] = np.cos(self.rotation_angle)
+        self.rotation_mat[0, 1] = -np.sin(self.rotation_angle)
+        self.rotation_mat[1, 0] = np.sin(self.rotation_angle)
+        self.rotation_mat[1, 1] = np.cos(self.rotation_angle)
+        self._update_mvp()
+        pass
+
+    def _update_mvp(self):
+        mvp = np.matmul(self.scale_mat, self.rotation_mat) 
+        mvp = np.matmul(mvp, self.translation_mat)
+        STATE.mvp = mvp
 
     def describe(self):
         with self:
             el.Text("Hello World!").construct()
-            el.Slider(self, 'scale').construct(width=200, height=20, callback=self._on_value_changed, min_value=0.1, max_value=10.0)
+            el.Slider(self, 'scale').construct(width=200, height=20, callback=self._on_scale_changed, min_value=0.1, max_value=10.0)
+            el.Slider(self, 'rotation_angle').construct(width=200, height=20, callback=self._on_rotation_changed, min_value=0.0, max_value=2*np.pi)
+            el.Slider(self, 'translation_x').construct(width=200, height=20, callback=self._on_translation_changed, min_value=-1.0, max_value=1.0)
+            el.Slider(self, 'translation_y').construct(width=200, height=20, callback=self._on_translation_changed, min_value=-1.0, max_value=1.0)
+
 
     
 
