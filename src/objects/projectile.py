e85e63a22da2391d70f4a4576ca5d40de5343e07
marcuscastelo <marcuscastelo@usp.br>
2022-04-27 23:21:42 -0300

    Re-enable projectile shattering

--- src/objects/projectile.py
+++ src/objects/projectile.py
@@ -34,11 +34,11 @@ class ProjectileSpecs(ElementSpecification):
 class Projectile(Element):
     def __init__(self, world: 'World', specs: ProjectileSpecs, *args, **kwargs):
         self.live_time = 0
-        self.projectile_specs = specs
+        self.specs = specs
         
         # TODO: avoid scale overwriting somehow (if user already set scale before)
-        specs.initial_transform.scale.y = specs.length
-        specs.shape_specs = [
+        self.specs.initial_transform.scale.y = self.specs.length
+        self.specs.shape_specs = [
             ShapeSpec(vertices=np.array([
                 *(0.0,  -1.0,   0.0),
                 *(0.0,  +1.0,   0.0),
@@ -47,10 +47,14 @@ class Projectile(Element):
             )
         ]
 
+        assert isinstance(self.specs.initial_speed, float), f"{self.specs.initial_speed} is not a float, but {type(self.specs.initial_speed)}"
+
         super().__init__(world, specs, *args, **kwargs)
+        self.specs = specs # TODO: HACK: this is a hack to reoverwrite the specs of the parent class (Element)
+        assert isinstance(self.specs.initial_speed, float), f"{self.specs.initial_speed} is not a float, but {type(self.specs.initial_speed)}"
 
         self.is_particle = True # TODO: remove this and create a proper particle class
-        self.speed = self.projectile_specs.initial_speed
+        self.speed = self.specs.initial_speed
 
     def _get_bounding_box_vertices(self) -> np.ndarray:
         return np.array([
@@ -59,7 +63,7 @@ class Projectile(Element):
         ])
 
     def _render(self):
-        gl.glLineWidth(self.projectile_specs.width)
+        gl.glLineWidth(self.specs.width)
         return super()._render()
 
     @classmethod
@@ -89,9 +93,9 @@ class Projectile(Element):
             return
 
         self.move_forward()
-        self.speed = max(self.speed + self.speed * self.projectile_specs.acceleration, 0)
+        self.speed = max(self.speed + self.speed * self.specs.acceleration, 0)
 
-        self.transform.scale.y *= (1 - self.projectile_specs.decay_rate)
+        self.transform.scale.y *= (1 - self.specs.decay_rate)
 
         screen = Rect2(-1, -1, 1, 1)
         outside_screen = not screen.contains(self.transform.translation.xy)
@@ -103,35 +107,35 @@ class Projectile(Element):
         return super()._physics_update(delta_time)
 
     def destroy(self):
-        # if not self.destroyed and not self.too_small() and not self.is_particle:
-        #     impact_xyz = self.transform.translation.xyz
-        #     TIME_TO_TRAVEL_SCREEN = 1 # seconds
-        #     MAX_PARTICLES = 50
-        #     particle_lifetime_completion = min(1, self.live_time / TIME_TO_TRAVEL_SCREEN)
+        if not self.destroyed and not self.too_small() and not self.is_particle:
+            impact_xyz = self.transform.translation.xyz
+            TIME_TO_TRAVEL_SCREEN = 1 # seconds
+            MAX_PARTICLES = 25
+            particle_lifetime_completion = min(1, self.live_time / TIME_TO_TRAVEL_SCREEN)
 
-        #     number_of_minibullets = int(math.ceil(MAX_PARTICLES * particle_lifetime_completion)) + 3
-        #     start_angle = self.transform.rotation.z
-        #     angle_step = 2 * math.pi / number_of_minibullets
+            number_of_minibullets = int(math.ceil(MAX_PARTICLES * particle_lifetime_completion)) + 3
+            start_angle = self.transform.rotation.z
+            angle_step = 2 * math.pi / number_of_minibullets
 
-        #     initial_speed=self.projectile_specs.initial_speed * 0.5,
-        #     decay_rate=self.projectile_specs.decay_rate * 4
+            initial_speed=self.specs.initial_speed * 0.5
+            decay_rate=self.specs.decay_rate * 4
             
-        #     mini_bullets = [
-        #         Projectile(
-        #             self.world, 
-        #             specs=ProjectileSpecs(
-        #                 initial_speed=initial_speed,
-        #                 decay_rate=decay_rate,
-        #                 initial_transform=Transform(
-        #                     translation=impact_xyz + Vec3(0, 0, 0.1), 
-        #                     rotation=Vec3(0, 0, start_angle + math.pi/2 + angle_step/2 * i)
-        #                 )
-        #             )
-        #         ) for i in range(number_of_minibullets)
-        #     ]
+            mini_bullets = [
+                Projectile(
+                    self.world, 
+                    specs=ProjectileSpecs(
+                        initial_speed=initial_speed,
+                        decay_rate=decay_rate,
+                        initial_transform=Transform(
+                            translation=impact_xyz + Vec3(0, 0, 0.1), 
+                            rotation=Vec3(0, 0, start_angle + math.pi/2 + angle_step/2 * i)
+                        )
+                    )
+                ) for i in range(number_of_minibullets)
+            ]
 
-        #     for bullet in mini_bullets:
-        #         bullet.transform.scale.y = 0.1
+            for bullet in mini_bullets:
+                bullet.transform.scale.y = 0.1
 
         return super().destroy()
 
