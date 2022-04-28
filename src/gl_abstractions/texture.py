1df1d80a12e8d2629a6311d31cff4b09a42a08f0
marcuscastelo <marcuscastelo@usp.br>
2022-04-27 23:53:46 -0300

    Create texture class

---
+++ src/gl_abstractions/texture.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from utils.sig import metsig
+
+from OpenGL import GL as gl
+import numpy as np
+
+class Texture:
+    def __init__(self):
+        self.id = gl.glGenTextures(1)
+        pass
+
+
+@dataclass
+class Texture2DParameters:
+    wrap_s: int = gl.GL_CLAMP_TO_BORDER
+    wrap_t: int = gl.GL_CLAMP_TO_BORDER
+    min_filter: int = gl.GL_LINEAR
+    mag_filter: int = gl.GL_LINEAR
+    internal_format: int = gl.GL_RGBA
+    format: int = gl.GL_RGBA
+    type: int = gl.GL_UNSIGNED_BYTE
+
+class Texture2D(Texture):
+    @metsig
+    def __init__(self, *args, tex2d_params: Texture2DParameters = None, **kwargs):
+        self.params = tex2d_params
+        super().__init__(*args, **kwargs)
+
+    def upload_raw_texture(self, texture: np.ndarray) -> None:
+        self.bind()
+        
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, self.params.wrap_s)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, self.params.wrap_t)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, self.params.min_filter)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, self.params.mag_filter)
+
+        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, self.params.internal_format, texture.shape[1], texture.shape[0], 0, self.params.format, self.params.type, texture)
+
+        self.unbind()
+        pass
+
+    def bind(self) -> None:
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.id)
+
+    def unbind(self) -> None:
+        gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
