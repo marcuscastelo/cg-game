fecd9aeaf81adc0ab4d9b65159bb5b187fb8d69e
marcuscastelo <marcuscastelo@usp.br>
2022-06-25 03:31:57 -0300

    Rework raycast element selection algorithm

--- src/ray.py
+++ src/ray.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from dis import dis
 
 from utils.geometry import Vec3
 from utils.logger import LOGGER
@@ -21,42 +22,78 @@ class Ray(Element):
         self.transform.translation.xyz += self.direction * delta_time * PHYSICS_TPS * 0.3
 
         from app_vars import APP_VARS
-        for element in APP_VARS.world.elements:
-            if element in [APP_VARS.camera, self, APP_VARS.camera.raycast_line_dbg]:
-                continue
-            
-            if element.name in ['Ground', 'light_cube']:
-                continue
 
+        selectable_elements = APP_VARS.world.elements
+        selectable_elements = [ element for element in selectable_elements if element not in [APP_VARS.camera, self, APP_VARS.camera.raycast_line_dbg] ]
+        selectable_elements = [ element for element in selectable_elements if element.name not in ['Ground', 'light_cube'] ]
+
+        def calc_distance(element: Element) -> float:
             difference = element.transform.translation.xyz - self.transform.translation.xyz
             scaled_difference = difference * element.transform.scale.xyz
-            if element._state.selected:
-                scaled_difference /= 2 # FIXME: hardcoded value
+            # if element._state.selected:
+            #     scaled_difference /= 2 # FIXME: hardcoded value
 
             distance = scaled_difference.magnitude()
+            return distance
 
-            DST = 1
-            keep_iterating = True
-            def stop_raycast():
+        distances = [ calc_distance(element) for element in selectable_elements ]
+
+        def stop_raycast():
                 self.direction = Vec3(0,0,0)
                 self.transform.translation.xyz = Vec3(0,100,0)
                 keep_iterating = False
 
-            if self.transform.translation.xyz.magnitude() > 30:
-                stop_raycast()
+        sorted_pairs = sorted(zip(distances, selectable_elements), key=lambda a: a[0])
+        if sorted_pairs:
+            element = sorted_pairs[0][1]
+            distance = sorted_pairs[0][0]
 
-            if distance < DST:
+
+
+            if distance < 1:
                 if element._state.selected:
                     element.unselect()
                 else:
                     element.select()
+                
                 stop_raycast()
-            else:
-                if element._state.selected:
-                    # element.destroy()
-                    # element.unselect()
-                    pass
 
-            if not keep_iterating:
-                break
+        # for element in APP_VARS.world.elements:
+        #     if element in [APP_VARS.camera, self, APP_VARS.camera.raycast_line_dbg]:
+        #         continue
+            
+        #     if element.name in ['Ground', 'light_cube']:
+        #         continue
+
+        #     difference = element.transform.translation.xyz - self.transform.translation.xyz
+        #     scaled_difference = difference * element.transform.scale.xyz
+        #     if element._state.selected:
+        #         scaled_difference /= 2 # FIXME: hardcoded value
+
+        #     distance = scaled_difference.magnitude()
+
+        #     DST = 1
+        #     keep_iterating = True
+        #     def stop_raycast():
+        #         self.direction = Vec3(0,0,0)
+        #         self.transform.translation.xyz = Vec3(0,100,0)
+        #         keep_iterating = False
+
+        #     if self.transform.translation.xyz.magnitude() > 30:
+        #         stop_raycast()
+
+        #     if distance < DST:
+        #         if element._state.selected:
+        #             element.unselect()
+        #         else:
+        #             element.select()
+        #         stop_raycast()
+        #     else:
+        #         if element._state.selected:
+        #             # element.destroy()
+        #             # element.unselect()
+        #             pass
+
+        #     if not keep_iterating:
+        #         break
         return super()._physics_update(delta_time)
