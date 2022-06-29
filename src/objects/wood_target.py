288c82272b539ec47a05c84e35b9cfc48baf8cfd
marcuscastelo <marcuscastelo@usp.br>
2022-06-29 09:25:56 -0300

    Fix WoodTarget Texture

--- src/objects/wood_target.py
+++ src/objects/wood_target.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import math
 
 from utils.geometry import Vec3
@@ -9,12 +9,19 @@ from wavefront.model import Model
 from wavefront.model_reader import ModelReader
 
 ALVO_2_MODEL = ModelReader().load_model_from_file('models/alvo2.obj')
-ALVO_2_TEXTURE = Texture2D.from_image_path('textures/wood.jpg')
+ALVO_2_TEXTURE = None 
+def get_tex():
+    # TODO: remove this and make a TextureDB and ModelDB
+    global ALVO_2_TEXTURE
+    if ALVO_2_TEXTURE is None:
+        ALVO_2_TEXTURE = Texture2D.from_image_path('textures/wood.jpg')
+
+    return ALVO_2_TEXTURE
 
 @dataclass
 class WoodTarget(Cube):
-    model: Model = ALVO_2_MODEL
-    texture: Texture = ALVO_2_TEXTURE
+    model: Model = field(default_factory= lambda: ALVO_2_MODEL)
+    texture: Texture = field(default_factory=get_tex)
     ray_selectable: bool = True
     ray_destroyable: bool = True
     def __post_init__(self):
